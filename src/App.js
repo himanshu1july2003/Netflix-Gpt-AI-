28db495805afb514a0ef76ae8bc481d2eb7c568e
Himanshu <himanshu1july2003@gmail.com>
2025-04-05 18:07:29 +0530

    sigup&signin

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="text-orange-300" >
+        <h1 class="text-3xl font-bold underline"> Hello worlddddddddddddd!</h1>
+        <p className>
+          Cheetaah hi kehde
+        </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],  // Ensure React files are included
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
